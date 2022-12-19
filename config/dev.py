f1ead4e81535cd7863c29077659145caa2008fec
Paul Marceau <paul@themarceaus.net>
2022-12-19 15:06:43 -0500

    New dev config file

---
+++ config/dev.py
@@ -0,0 +1,6 @@
+# dev config settings
+
+DEBUG = True
+SECRET_KEY = 'password'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost/flask_for_begginers'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
